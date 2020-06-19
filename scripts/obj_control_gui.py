3769e6eda3e436d2063bdd8f9dc0892642875b45
Adnan Munawar <adnan.munawar@live.com>
2020-06-19 19:28:15 -0400

    Incorporation the OBJ control GUIs in the test script

---
+++ scripts/obj_control_gui.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python
+# //==============================================================================
+# /*
+#     Software License Agreement (BSD License)
+#     Copyright (c) 2019
+#     (aimlab.wpi.edu)
+
+#     All rights reserved.
+
+#     Redistribution and use in source and binary forms, with or without
+#     modification, are permitted provided that the following conditions
+#     are met:
+
+#     * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+
+#     * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+
+#     * Neither the name of authors nor the names of its contributors may
+#     be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+
+#     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#     "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#     FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#     COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#     INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#     BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#     CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#     LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#     POSSIBILITY OF SUCH DAMAGE.
+
+#     \author    <aimlab.wpi.edu>
+#     \author    <amunawar@wpi.edu>
+#     \author    Adnan Munawar
+#     \version   1.0
+# */
+# //==============================================================================
+from Tkinter import *
+
+
+class ObjectGUI:
+    def __init__(self, obj_name, initial_xyz, initial_rpy, range_xyz, range_rpy, resolution):
+        self.App = Tk()
+        if initial_xyz:
+            self.initial_xyz = initial_xyz
+        else:
+            self.initial_xyz = [0.0, 0.0, 0.0]
+
+        if initial_rpy:
+            self.initial_rpy = initial_rpy
+        else:
+            self.initial_rpy = [0, 0, 0]
+
+        if range_xyz:
+            self.range_xyz = range_xyz
+        else:
+            self.range_xyz = 1.0
+
+        if range_rpy:
+            self.range_rpy = range_rpy
+        else:
+            self.range_rpy = 3.14
+
+        if resolution:
+            self.resolution = resolution
+        else:
+            self.resolution = 0.0001
+
+        self.x = self.initial_xyz[0]
+        self.y = self.initial_xyz[1]
+        self.z = self.initial_xyz[2]
+        self.ro = self.initial_rpy[0]
+        self.pi = self.initial_rpy[1]
+        self.ya = self.initial_rpy[2]
+        self.gr = 0.0
+        self.x_slider = None
+        self.y_slider = None
+        self.z_slider = None
+        self.ro_slider = None
+        self.pi_slider = None
+        self.ya_slider = None
+        self.gr_slider = None
+
+        self.cartesian_mode = 0
+        self.obj_name = obj_name
+        self.create_gui(self.App, self.obj_name)
+
+    def set_init_xyz(self, xyz):
+        self.initial_xyz = xyz
+        self.x = self.initial_xyz[0]
+        self.y = self.initial_xyz[1]
+        self.z = self.initial_xyz[2]
+        print self.initial_xyz
+        self.App.quit()
+        self.App.destroy()
+        self.App = Tk()
+        self.create_gui(self.App, self.obj_name)
+
+    def set_init_rpy(self, rpy):
+        self.initial_rpy = rpy
+        self.App.quit()
+        self.App.destroy()
+        del self.App
+        self.App = Tk()
+        self.create_gui(self.App, self.obj_name)
+
+    # Def Init Function
+    def get_app_handle(self):
+        return self.App
+
+    # Define Callbacks for Tkinter GUI Sliders
+    def x_cb(self, val):
+        self.x = float(val)
+
+    def y_cb(self, val):
+        self.y = float(val)
+
+    def z_cb(self, val):
+        self.z = float(val)
+
+    def roll_cb(self, val):
+        self.ro = float(val)
+
+    def pitch_cb(self, val):
+        self.pi = float(val)
+
+    def yaw_cb(self, val):
+        self.ya = float(val)
+
+    def gr_cb(self, val):
+        self.gr = float(val)
+
+    def zero_all_cb(self):
+        self.zero_xyz_cb()
+        self.zero_rpy_cb()
+
+    def zero_xyz_cb(self):
+        self.x = self.initial_xyz[0]
+        self.y = self.initial_xyz[1]
+        self.z = self.initial_xyz[2]
+        self.x_slider.set(self.x)
+        self.y_slider.set(self.y)
+        self.z_slider.set(self.z)
+
+    def zero_rpy_cb(self):
+        self.ro = self.initial_rpy[0]
+        self.pi = self.initial_rpy[1]
+        self.ya = self.initial_rpy[2]
+        self.ro_slider.set(self.ro)
+        self.pi_slider.set(self.pi)
+        self.ya_slider.set(self.ya)
+
+    def create_gui(self, app, obj_name):
+        _width = 20
+        _length = 300
+        _resolution = 0.0001
+        # Define Sliders and Labels
+
+        row_count = 0
+        obj_label = Label(app, text='CONTROLLING OBJECT: ' + obj_name, fg="Red")
+        obj_label.grid(row=row_count, columnspan=2, pady=5)
+
+        row_count = row_count + 1
+
+        min_v = self.initial_xyz[0] - self.range_xyz / 2.0
+        max_v = self.initial_xyz[0] + self.range_xyz / 2.0
+        self.x_slider = Scale(app, from_=min_v, to=max_v, resolution=self.resolution, width=_width, length=_length, orient=HORIZONTAL,
+                         command=self.x_cb)
+        self.x_slider.grid(row=row_count, column=1)
+
+        self.x_slider.set(self.x)
+
+        row_count = row_count + 1
+
+        x_label = Label(app, text="x")
+        x_label.grid(row=row_count, column=1, pady=5)
+
+        row_count = row_count + 1
+
+        min_v = self.initial_xyz[1] - self.range_xyz / 2.0
+        max_v = self.initial_xyz[1] + self.range_xyz / 2.0
+        self.y_slider = Scale(app, from_=min_v, to=max_v, resolution=self.resolution, width=_width, length=_length, orient=HORIZONTAL,
+                         command=self.y_cb)
+        self.y_slider.grid(row=row_count, column=1)
+
+        self.y_slider.set(self.y)
+
+        row_count = row_count + 1
+
+        y_label = Label(app, text="y")
+        y_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+        min_v = self.initial_xyz[2] - self.range_xyz / 2.0
+        max_v = self.initial_xyz[2] + self.range_xyz / 2.0
+        self.z_slider = Scale(app, from_=min_v, to=max_v, resolution=self.resolution, width=_width, length=_length, orient=HORIZONTAL,
+                         command=self.z_cb)
+        self.z_slider.grid(row=row_count, column=1)
+        self.z_slider.set(self.z)
+
+        row_count = row_count + 1
+
+        z_label = Label(app, text="z")
+        z_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        zero_xyz = Button(app, width=_width, command=self.zero_xyz_cb)
+        zero_xyz.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        zero_xyz_label = Label(app, text="Reset Position")
+        zero_xyz_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        min_v = self.initial_rpy[0] - self.range_rpy / 2.0
+        max_v = self.initial_rpy[0] + self.range_rpy / 2.0
+        self.ro_slider = Scale(app, from_=min_v, to=max_v, resolution=self.resolution, width=_width, length=_length, orient=HORIZONTAL,
+                               command=self.roll_cb)
+        self.ro_slider.grid(row=row_count, column=1)
+        self.ro_slider.set(self.ro)
+
+        row_count = row_count + 1
+
+        roll_label = Label(app, text="roll")
+        roll_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        min_v = self.initial_rpy[1] - self.range_rpy / 2.0
+        max_v = self.initial_rpy[1] + self.range_rpy / 2.0
+        self.pi_slider = Scale(app, from_=min_v, to=max_v, resolution=self.resolution, width=_width, length=_length, orient=HORIZONTAL,
+                               command=self.pitch_cb)
+        self.pi_slider.grid(row=row_count, column=1)
+        self.pi_slider.set(self.pi)
+
+        row_count = row_count + 1
+
+        pitch_label = Label(app, text="pitch")
+        pitch_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        min_v = self.initial_rpy[2] - self.range_rpy / 2.0
+        max_v = self.initial_rpy[2] + self.range_rpy / 2.0
+        self.ya_slider = Scale(app, from_=min_v, to=max_v, resolution=self.resolution, width=_width, length=_length, orient=HORIZONTAL,
+                               command=self.yaw_cb)
+        self.ya_slider.grid(row=row_count, column=1)
+        self.ya_slider.set(self.ya)
+
+        row_count = row_count + 1
+
+        yaw_label = Label(app, text="yaw")
+        yaw_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        zero_rpy = Button(app, width=_width, command=self.zero_rpy_cb)
+        zero_rpy.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        zero_rpy_label = Label(app, text="Reset Rotation")
+        zero_rpy_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        zero_all = Button(app, width=_width, command=self.zero_all_cb)
+        zero_all.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        zero_all_label = Label(app, text="Reset All")
+        zero_all_label.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        self.gr_slider = Scale(app, from_=0, to=1.0, resolution=self.resolution, width=_width, length=_length,
+                               orient=HORIZONTAL,
+                               command=self.gr_cb)
+        self.gr_slider.grid(row=row_count, column=1)
+
+        row_count = row_count + 1
+
+        gr_label = Label(app, text="Gripper")
+        gr_label.grid(row=row_count, column=1)
--- scripts/testIK.py
+++ scripts/testIK.py
@@ -1,33 +1,54 @@
+#!/usr/bin/env python
+# //==============================================================================
+# /*
+#     Software License Agreement (BSD License)
+#     Copyright (c) 2019
+#     (aimlab.wpi.edu)
+
+#     All rights reserved.
+
+#     Redistribution and use in source and binary forms, with or without
+#     modification, are permitted provided that the following conditions
+#     are met:
+
+#     * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+
+#     * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+
+#     * Neither the name of authors nor the names of its contributors may
+#     be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+
+#     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#     "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#     FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#     COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#     INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#     BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#     CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#     LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#     POSSIBILITY OF SUCH DAMAGE.
+
+#     \author    <aimlab.wpi.edu>
+#     \author    <amunawar@wpi.edu>
+#     \author    Adnan Munawar
+#     \version   1.0
+# */
+# //==============================================================================
 from psmIK import *
 from ambf_client import Client
 import time
 import rospy
-from Tkinter import *
 from PyKDL import Frame, Rotation, Vector
 from argparse import ArgumentParser
-
-class JawControlGUI:
-    def __init__(self):
-        self.App = Tk()
-        self.min = 0.0
-        self.max = 1.0
-        self.resolution = 0.001
-        self.slider1_val = 0.0
-        self.slider2_val = 0.0
-
-        self.jaw1_slider = Scale(self.App, from_=self.min, to=self.max, resolution=self.resolution,
-                                 orient=HORIZONTAL, command=self.slider1_cb)
-        self.jaw1_slider.grid(row=0, column=0)
-
-        self.jaw1_slider = Scale(self.App, from_=self.min, to=self.max, resolution=self.resolution,
-                                 orient=HORIZONTAL, command=self.slider2_cb)
-        self.jaw1_slider.grid(row=1, column=0)
-
-    def slider1_cb(self, val):
-        self.slider1_val = float(val)
-
-    def slider2_cb(self, val):
-        self.slider2_val = float(val)
+import obj_control_gui
 
 
 def test_ik():
@@ -43,98 +64,135 @@ def test_ik():
     print('Comp Q: ', computed_q)
 
 
-def test_ambf_psm(run_psm_one, run_psm_two):
-    c = Client()
-    c.connect()
+class TestPSMIK:
+    def __init__(self, run_psm_one, run_psm_two, OG1=None, OG2=None):
+        self.c = Client()
+        self.c.connect()
 
-    base1 = None
-    base2 = None
-    target1 = None
-    target2 = None
+        self.base1 = None
+        self.base2 = None
+        self.target1 = None
+        self.target2 = None
+        self.run_psm_one = run_psm_one
+        self.run_psm_two = run_psm_two
 
-    if run_psm_one is True:
-        print('PREPARING TO LOAD IK FOR PSM1')
-        base1 = c.get_obj_handle('psm1/baselink')
-        target1 = c.get_obj_handle('psm1/target')
+        self.obj_gui1 = None
+        self.obj_gui2 = None
 
-    if run_psm_two is True:
-        print('PREPARING TO LOAD IK FOR PSM2')
-        base2 = c.get_obj_handle('psm2/baselink')
-        target2 = c.get_obj_handle('psm2/target')
+        time.sleep(1.0)
 
-    if not run_psm_one and not run_psm_two:
-        print('YOU HAVE TO RUN ATLEAST ONE PSMS IK FOR THIS SCRIPT TO DO ANYTHING')
-        return False
-    else:
-        jaw_ctrl = JawControlGUI()
+        if self.run_psm_one is True:
+            print('PREPARING TO LOAD IK FOR PSM1')
+            self.base1 = self.c.get_obj_handle('psm1/baselink')
+            self.target1 = self.c.get_obj_handle('psm1/target')
+            self.obj_gui1 = OG1
+            base_pos = self.base1.get_pos()
+            print "Base Pos 1"
+            print base_pos
+            init_xyz = [base_pos.x, base_pos.y, base_pos.z-0.15]
+            self.obj_gui1.set_init_xyz(init_xyz)
 
-    time.sleep(1.0)
+        if self.run_psm_two is True:
+            print('PREPARING TO LOAD IK FOR PSM2')
+            self.base2 = self.c.get_obj_handle('psm2/baselink')
+            self.target2 = self.c.get_obj_handle('psm2/target')
+            self.obj_gui2 = OG2
+            base_pos = self.base2.get_pos()
+            print "Base Pos 2"
+            print base_pos
+            init_xyz = [base_pos.x, base_pos.y, base_pos.z-0.15]
+            self.obj_gui2.set_init_xyz(init_xyz)
 
-    # The following are the names of the controllable joints.
-    #  'baselink-yawlink', 0
-    #  'yawlink-pitchbacklink', 1
-    #  'pitchendlink-maininsertionlink', 2
-    #  'maininsertionlink-toolrolllink', 3
-    #  'toolrolllink-toolpitchlink', 4
-    #  'toolpitchlink-toolgripper1link', 5a
-    #  'toolpitchlink-toolgripper2link', 5b
+        if not run_psm_one and not run_psm_two:
+            print('YOU HAVE TO RUN ATLEAST ONE PSMS IK FOR THIS SCRIPT TO DO ANYTHING')
 
-    while not rospy.is_shutdown():
-        jaw_ctrl.App.update()
+        # The following are the names of the controllable joints.
+        #  'baselink-yawlink', 0
+        #  'yawlink-pitchbacklink', 1
+        #  'pitchendlink-maininsertionlink', 2
+        #  'maininsertionlink-toolrolllink', 3
+        #  'toolrolllink-toolpitchlink', 4
+        #  'toolpitchlink-toolgripper1link', 5a
+        #  'toolpitchlink-toolgripper2link', 5b
 
-        if run_psm_one is True:
-            jaw1_val = jaw_ctrl.slider1_val
-            p = base1.get_pos()
-            q = base1.get_rot()
-            P_b_w = Vector(p.x, p.y, p.z)
-            R_b_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
-            T_b_w = Frame(R_b_w, P_b_w)
-            p = target1.get_pos()
-            q = target1.get_rot()
-            P_t_w = Vector(p.x, p.y, p.z)
-            R_t_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
-            T_t_w = Frame(R_t_w, P_t_w)
-            T_t_b = T_b_w.Inverse() * T_t_w
-            computed_q = compute_IK(T_t_b)
+    def run(self):
+        while not rospy.is_shutdown():
 
-            # print('SETTING JOINTS: ')
-            # print(computed_q)
+            if self.run_psm_one is True:
+                self.obj_gui1.App.update()
+                # Move the Target Position Based on the GUI
+                x = self.obj_gui1.x
+                y = self.obj_gui1.y
+                z = self.obj_gui1.z
+                ro = self.obj_gui1.ro
+                pi = self.obj_gui1.pi
+                ya = self.obj_gui1.ya
+                gr = self.obj_gui1.gr
+                self.target1.set_pos(x, y, z)
+                self.target1.set_rpy(ro, pi, ya)
 
-            base1.set_joint_pos('baselink-yawlink', computed_q[0])
-            base1.set_joint_pos('yawlink-pitchbacklink', computed_q[1])
-            base1.set_joint_pos('pitchendlink-maininsertionlink', computed_q[2])
-            base1.set_joint_pos('maininsertionlink-toolrolllink', computed_q[3])
-            base1.set_joint_pos('toolrolllink-toolpitchlink', computed_q[4])
-            base1.set_joint_pos('toolpitchlink-toolgripper1link', -computed_q[5]+jaw1_val)
-            base1.set_joint_pos('toolpitchlink-toolgripper2link', computed_q[5]+jaw1_val)
+                p = self.base1.get_pos()
+                q = self.base1.get_rot()
+                P_b_w = Vector(p.x, p.y, p.z)
+                R_b_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
+                T_b_w = Frame(R_b_w, P_b_w)
+                p = self.target1.get_pos()
+                q = self.target1.get_rot()
+                P_t_w = Vector(p.x, p.y, p.z)
+                R_t_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
+                T_t_w = Frame(R_t_w, P_t_w)
+                T_t_b = T_b_w.Inverse() * T_t_w
+                computed_q = compute_IK(T_t_b)
 
-        if run_psm_two is True:
-            jaw2_val = jaw_ctrl.slider2_val
-            p = base2.get_pos()
-            q = base2.get_rot()
-            P_b_w = Vector(p.x, p.y, p.z)
-            R_b_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
-            T_b_w = Frame(R_b_w, P_b_w)
-            p = target2.get_pos()
-            q = target2.get_rot()
-            P_t_w = Vector(p.x, p.y, p.z)
-            R_t_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
-            T_t_w = Frame(R_t_w, P_t_w)
-            T_t_b = T_b_w.Inverse() * T_t_w
-            computed_q = compute_IK(T_t_b)
+                # print('SETTING JOINTS: ')
+                # print(computed_q)
 
-            # print('SETTING JOINTS: ')
-            # print(computed_q)
+                self.base1.set_joint_pos('baselink-yawlink', computed_q[0])
+                self.base1.set_joint_pos('yawlink-pitchbacklink', computed_q[1])
+                self.base1.set_joint_pos('pitchendlink-maininsertionlink', computed_q[2])
+                self.base1.set_joint_pos('maininsertionlink-toolrolllink', computed_q[3])
+                self.base1.set_joint_pos('toolrolllink-toolpitchlink', computed_q[4])
+                self.base1.set_joint_pos('toolpitchlink-toolgripper1link', -computed_q[5]+gr)
+                self.base1.set_joint_pos('toolpitchlink-toolgripper2link', computed_q[5]+gr)
 
-            base2.set_joint_pos('baselink-yawlink', computed_q[0])
-            base2.set_joint_pos('yawlink-pitchbacklink', computed_q[1])
-            base2.set_joint_pos('pitchendlink-maininsertionlink', computed_q[2])
-            base2.set_joint_pos('maininsertionlink-toolrolllink', computed_q[3])
-            base2.set_joint_pos('toolrolllink-toolpitchlink', computed_q[4])
-            base2.set_joint_pos('toolpitchlink-toolgripper1link', -computed_q[5]+jaw2_val)
-            base2.set_joint_pos('toolpitchlink-toolgripper2link', computed_q[5]+jaw2_val)
+            if self.run_psm_two is True:
+                self.obj_gui2.App.update()
+                # Move the Target Position Based on the GUI
+                x = self.obj_gui2.x
+                y = self.obj_gui2.y
+                z = self.obj_gui2.z
+                ro = self.obj_gui2.ro
+                pi = self.obj_gui2.pi
+                ya = self.obj_gui2.ya
+                gr = self.obj_gui2.gr
+                self.target2.set_pos(x, y, z)
+                self.target2.set_rpy(ro, pi, ya)
 
-        time.sleep(0.05)
+                p = self.base2.get_pos()
+                q = self.base2.get_rot()
+                P_b_w = Vector(p.x, p.y, p.z)
+                R_b_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
+                T_b_w = Frame(R_b_w, P_b_w)
+                p = self.target2.get_pos()
+                q = self.target2.get_rot()
+                P_t_w = Vector(p.x, p.y, p.z)
+                R_t_w = Rotation.Quaternion(q.x, q.y, q.z, q.w)
+                T_t_w = Frame(R_t_w, P_t_w)
+                T_t_b = T_b_w.Inverse() * T_t_w
+                computed_q = compute_IK(T_t_b)
+
+                # print('SETTING JOINTS: ')
+                # print(computed_q)
+
+                self.base2.set_joint_pos('baselink-yawlink', computed_q[0])
+                self.base2.set_joint_pos('yawlink-pitchbacklink', computed_q[1])
+                self.base2.set_joint_pos('pitchendlink-maininsertionlink', computed_q[2])
+                self.base2.set_joint_pos('maininsertionlink-toolrolllink', computed_q[3])
+                self.base2.set_joint_pos('toolrolllink-toolpitchlink', computed_q[4])
+                self.base2.set_joint_pos('toolpitchlink-toolgripper1link', -computed_q[5]+gr)
+                self.base2.set_joint_pos('toolpitchlink-toolgripper2link', computed_q[5]+gr)
+
+            time.sleep(0.005)
 
 
 if __name__ == "__main__":
@@ -146,4 +204,20 @@ if __name__ == "__main__":
     print('Specified Arguments')
     print parsed_args
 
-    test_ambf_psm(parsed_args.run_psm_one, parsed_args.run_psm_two)
+    OG1 = None
+    OG2 = None
+
+    if parsed_args.run_psm_one is True:
+        # Initial Target Offset for PSM1
+        psm1_xyz = [0.0, 0.0, 0.0]
+        psm1_rpy = [3.14, 0, -1.57079]
+        OG1 = obj_control_gui.ObjectGUI('psm1/baselink', psm1_xyz, psm1_rpy, 1.0, 3.14, 0.0001)
+
+    if parsed_args.run_psm_two is True:
+        # Initial Target Offset for PSM2
+        psm2_xyz = [0.0, 0.0, 0.0]
+        psm2_rpy = [3.14, 0, -1.57079]
+        OG2 = obj_control_gui.ObjectGUI('psm2/baselink', psm2_xyz, psm2_rpy, 1.0, 3.14, 0.0001)
+
+    psmIK = TestPSMIK(parsed_args.run_psm_one, parsed_args.run_psm_two, OG1, OG2)
+    psmIK.run()
