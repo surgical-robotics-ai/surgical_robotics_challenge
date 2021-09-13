380e40b2f1a264e47740691857d34870ea09e179
Adnan Munawar <adnan.munawar@live.com>
2021-09-13 11:00:20 -0400

    Moved the velocity limiting code to Cartesian command.

--- scripts/ecm_arm.py
+++ scripts/ecm_arm.py
@@ -63,6 +63,7 @@ class ECM:
         self._update_camera_pose()
         self._T_c_w_init = self._T_c_w
         self._measured_jp = np.array([.0, .0, .0, .0])
+        self._measured_cp = None
         self._max_vel = 0.002
 
     def is_present(self):
@@ -99,8 +100,32 @@ class ECM:
         if type(T_c_w) in [np.matrix, np.array]:
             T_c_w = convert_mat_to_frame(T_c_w)
 
-        self.camera_handle.set_pos(T_c_w.p[0], T_c_w.p[1], T_c_w.p[2])
-        rpy = T_c_w.M.GetRPY()
+        if self._measured_cp is None:
+            self._measured_cp = T_c_w
+        pe = T_c_w.p - self._measured_cp.p
+        len_pe = pe.Norm()
+        pe.Normalize()
+        if len_pe > self._max_vel:
+            p_cmd = pe * self._max_vel
+        else:
+            p_cmd = pe * len_pe
+
+        # Checl for Rotation
+        R_diff = self._measured_cp.M.Inverse() * T_c_w.M
+        re = Vector(R_diff.GetRPY()[0], R_diff.GetRPY()[1], R_diff.GetRPY()[2])
+        len_re = re.Norm()
+        re.Normalize()
+        if len_re > self._max_vel:
+            r_cmd = re * self._max_vel
+        else:
+            r_cmd = re * len_re
+
+        T_cmd = Frame()
+        T_cmd.p = self._measured_cp.p + p_cmd
+        T_cmd.M = self._measured_cp.M * Rotation.RPY(r_cmd[0], r_cmd[1], r_cmd[2])
+        self._measured_cp = T_cmd
+        self.camera_handle.set_pos(T_cmd.p[0], T_cmd.p[1], T_cmd.p[2])
+        rpy = T_cmd.M.GetRPY()
         self.camera_handle.set_rpy(rpy[0], rpy[1], rpy[2])
         self._pose_changed = True
 
@@ -124,15 +149,7 @@ class ECM:
         j1 = jp[1]
         j2 = jp[2]
         j3 = jp[3]
-        target_jp = np.array([j0, j1, j2, j3])
-        cmd = [0.0, 0.0, 0.0, 0.0, 0.0] # Add 0 to compute the added fifth frame
-        dp = target_jp - self._measured_jp
-        for i in range(len(dp)):
-            if abs(dp[i]) <= self._max_vel:
-                cmd[i] = self._measured_jp[i] + dp[i]
-            else:
-                cmd[i] = self._measured_jp[i] + (float(np.sign(dp[i])) * self._max_vel)
-        self._measured_jp = cmd[0:4]
+        cmd = [j0, j1, j2, j3, 0.0]
         T_t_c = convert_mat_to_frame(compute_FK(cmd)) # Tip if camera frame
         self.servo_cp(self._T_c_w_init * T_t_c)
 
