c63d8b23d67601518e2ed9c128be47295cb14814
Hisashi Ishida <hishida3@jhu.edu>
2022-03-03 17:38:47 -0500

    Create communication_loss environmnet

---
+++ scripts/surgical_robotics_challenge/teleoperation/communication_loss_publisher.py
@@ -0,0 +1,58 @@
+import rospy
+from std_msgs.msg import Bool
+
+def publisher():
+    pub = rospy.Publisher('communication_loss', Bool, queue_size=1)
+    rospy.init_node('communication_loss_talker', anonymous=True)
+
+    i = 0
+    flag = False
+
+    time_period = 4
+    loss_period = 1
+    
+    hz = 200
+    rate = rospy.Rate(hz) # 200hz
+
+    print("TIME_period: " + str(time_period) + " sec")
+    print("LOSS_period: "  + str(loss_period) + " sec")
+
+    while not rospy.is_shutdown():
+        i += 1
+
+        if (i == int(time_period - loss_period) * hz):
+            flag = True
+            pub.publish(flag)
+
+        if (i == time_period * hz):
+            flag = False
+            pub.publish(flag)
+            i = 0
+
+        #pub.publish(flag)
+        rate.sleep()
+
+    # now = rospy.get_time()
+    # flag = False
+    # while not rospy.is_shutdown():
+    #
+    #     if (rospy.get_time() - now > 10):
+    #         flag = True
+    #
+    #     if (rospy.get_time() - now > 12):
+    #         flag = False
+    #         now = rospy.get_time()
+    #
+    #     pub.publish(flag)
+    #     rate.sleep()
+
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        publisher()
+    except rospy.ROSInterruptException:
+        pass
